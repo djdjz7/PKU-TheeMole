bd39b993ff6a20d0b025b7f67f47c43cb85218a4
djdjz7 <djdjz7@icloud.com>
2024-08-08 12:51:15 +0800

    update js ver

--- stay-inject.js
+++ stay-inject.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         PKU TheeMole
 // @namespace    https://staybrowser.com/
-// @version      0.1
+// @version      0.2
 // @description  Optimized Treehole UI for Stay
 // @author       You
 // @match        *://treehole.pku.edu.cn/*
